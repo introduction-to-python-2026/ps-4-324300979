a38aa2c384680ebdcf5f98a93b4cb4c6f2bf954f
324300979 <yaelarbel2002@gmail.com>
2025-11-15 19:36:17 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,17 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    new_list = []
+    prev_upper = 0
+    if formula != "":
+       for i in range(1,len(formula)):
+           if formula[i].isupper():
+              new_list.append(formula[prev_upper:i])
+              prev_upper = i
+       new_list.append(formula[prev_upper:len(formula)])
+    return new_list
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    for i in range(len(formula)):
+        if formula[i].isdigit():
+          return (formula[:i:], int(formula[i::]))
+    return (formula, 1)
